3262dbf36cf1b6471477a64a00dea24ddd6694e9
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2022-10-19 15:54:39 +0530

    Time: 12 ms (7.04%), Space: 6.6 MB (27.54%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(st.size()==0)
+                st.push(s[i]);
+            else
+            {
+                char ch  = s[i];
+                if(ch>='A'&&ch<='Z')// check if this character is upper 
+                {
+                  char temp = tolower(ch);
+                  if(st.top()==temp)
+                        st.pop();
+                  else
+                        st.push(ch);
+                }
+                else // it is lower character
+                {
+                    char temp  = toupper(ch);
+                    if(st.top()==temp)
+                        st.pop();
+                    else
+                        st.push(ch);
+                }
+            }
+            
+        }
+        string ans="";
+        while(st.size()!=0)
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+    }
+};
