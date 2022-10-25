716a5a472a9037df6f550fb37d8866497b2f649b
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2022-10-25 21:21:00 +0530

    Time: 16 ms (90.09%), Space: 10.4 MB (27.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -18,11 +18,31 @@ public:
         int n = matrix.size();
         int m = n;
         int ans = INT_MAX;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++)
+            dp[0][i]=matrix[0][i];
+        for(int i=1;i<n;i++)
+        {
+            for(int j =0;j<n;j++)
+            {
+                int down = matrix[i][j] + dp[i-1][j];
+                int leftdig = 1e5;
+                if(j-1>=0)
+                    leftdig = matrix[i][j] + dp[i-1][j-1];
+                int rightdig = 1e5;
+                if(j+1<n)
+                    rightdig = matrix[i][j] + dp[i-1][j+1];
+                dp[i][j] = min(down, min(leftdig, rightdig));
+            }
+            
+        }
+        
+            
+              
+        
         for(int i=0;i<n;i++)
         {
-            int temp = totalways(n-1, i,matrix,dp);
-            ans = min(temp, ans);
+            ans = min(ans, dp[n-1][i]);
         }
         return ans;
         
