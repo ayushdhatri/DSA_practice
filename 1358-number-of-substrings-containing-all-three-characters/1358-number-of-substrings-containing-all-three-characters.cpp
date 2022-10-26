536477a24c458d3d1f10eb41f067e112055f03cd
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2022-10-26 17:57:25 +0530

    Time: 67 ms (58.58%), Space: 8.6 MB (55.93%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool exactone(vector<int>&arr)
+    {
+        if(arr[0]==1&&arr[1]==1&&arr[2]==1)
+            return true;
+        return false;
+        
+    }
+    bool atleastone(vector<int>&arr)
+    {
+       if(arr[0]==0||arr[1]==0||arr[2]==0)
+           return false;
+        return true;
+        
+    }
+    int numberOfSubstrings(string s) {
+        vector<int>arr(26,0);
+        int i=0,j=0;
+        int n = s.size();
+        int count=0;
+        while(j<n)
+        {
+            arr[s[j]-'a']++;
+            if(atleastone(arr)==false)
+                j++;
+            else
+            {
+                
+                while(atleastone(arr)!=false)
+                {
+                    count+=(n-j);
+                    arr[s[i]-'a']--;
+                    i++;
+                }
+                if(atleastone(arr)==true)
+                    count++;
+                j++;
+            }
+        }
+        return count;
+        
+        
+    }
+};
