ece5cccfea0dd741652ebfa118fc1f8d5cb0c884
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2022-10-28 10:00:43 +0530

    Time: 40 ms (91.12%), Space: 20.6 MB (63.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int  n = strs.size();
-        vector<pair<string, string>>words;
-        for(int i=0;i<n;i++) //Tc = n^2logn
-        {
-            string sortedword = strs[i];
-            sort(sortedword.begin(), sortedword.end());
-            words.push_back({strs[i], sortedword});
-        }
-        vector<vector<string>>ans;
-        vector<bool>visited(n,false);
+        unordered_map<string, vector<string>>ana;
         for(int i=0;i<n;i++)
         {
-            if(visited[i]==true)continue;
-            vector<string>temp;
-            temp.push_back(strs[i]);
-            for(int j=i+1;j<n;j++)
-            {
-                if(words[j].second==words[i].second) {
-                    temp.push_back(strs[j]);
-                    visited[j]=true;
-                }
-             }
-            ans.push_back(temp);
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            ana[temp].push_back(strs[i]);
+        }
+        vector<vector<string>>ans;
+        for(auto x: ana)
+        {
+            ans.push_back(x.second);
         }
         
         
