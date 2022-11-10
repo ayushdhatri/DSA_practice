69479063897f55643abbe6d1dbd0b583b9f2da6f
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2022-11-11 00:24:25 +0530

    Time: 248 ms (25.05%), Space: 45.3 MB (25.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,52 +1,32 @@
 class Solution {
 public:
-    int mincoins(int index, vector<int>&coins, int amount, vector<vector<int>>&dp)
+    int mincoins(vector<int>&coins, int amount, int index, vector<vector<int>>&dp)
     {
         if(amount==0)
             return 0;
+            
         if(index==0)
         {
-            if(amount%coins[index]==0)
-                return amount/coins[index];
+            if(amount%coins[0]==0)
+                return amount/coins[0];
             return 1e5;
         }
         if(dp[index][amount]!=-1)
             return dp[index][amount];
         int pick = 1e5;
         if(amount>=coins[index])
-             pick = 1 + mincoins(index, coins, amount-coins[index],dp);
-        int notpick = 0;
-        notpick = mincoins(index-1, coins, amount,dp);
+          pick=1+ mincoins(coins,amount-coins[index], index,dp);
+        int notpick = 0 + mincoins(coins, amount, index-1,dp);
         return dp[index][amount] = min(pick, notpick);
-        
     }
     int coinChange(vector<int>& coins, int amount) {
+        
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, 0));
-        // for(int i=0;i<n;i++)
-        //     dp[0][i]=0;
-        for(int i=0;i<=amount;i++)
-        {
-            if(i%coins[0]==0)
-                dp[0][i]=i/coins[0];
-            else
-                dp[0][i]=1e5;
-            
-        }
-        for(int index = 1;index<n;index++)
-        {
-            for(int sikka = 0;sikka<=amount;sikka++)
-            {
-                int pick = 1e5;
-                if(sikka>=coins[index])
-                    pick = 1 + dp[index][sikka-coins[index]];
-              //  int notpick = 1e5;
-                int notpick = dp[index-1][sikka];
-                dp[index][sikka]=min(pick, notpick);
-            }
-        }
-        if(dp[n-1][amount]>=1e5)
+        vector<vector<int>>dp(n,vector<int>(amount+1, -1));
+        int temp = mincoins(coins, amount,n-1,dp);
+        if(temp>=1e5)
             return -1;
-        return dp[n-1][amount];
+        return temp;
+        
     }
 };
