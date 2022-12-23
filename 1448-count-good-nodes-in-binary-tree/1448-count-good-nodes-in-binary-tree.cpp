9f2bccbc63313baa13c19ca763882935e9ff98a2
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2022-12-23 13:42:19 +0530

    Time: 329 ms (12.16%), Space: 86.3 MB (59.19%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,30 +11,23 @@
  */
 class Solution {
 public:
-    void nodes(TreeNode* root, vector<int>path, int &count)
+    void nodes(TreeNode* root, int maxSoFar, int &count)
     {
         if(root==NULL)
             return;
         int currnode = root->val;
-        path.push_back(currnode);
-        bool include = true;
-        for(auto x: path)
+        if(currnode>=maxSoFar)
         {
-            if(x>currnode) {
-                include = false;
-                break;
-            }
-        }
-        if(include)
             count++;
-        nodes(root->left, path, count);
-        nodes(root->right, path, count);
+            maxSoFar = currnode;
+        }
+        nodes(root->left, maxSoFar, count);
+        nodes(root->right, maxSoFar, count);
     }
-    
     int goodNodes(TreeNode* root) {
-        vector<int>path;
+        int maxSoFar = INT_MIN;
         int count=0;
-        nodes(root, path, count);
+        nodes(root, maxSoFar, count);
         return count;
         
         
