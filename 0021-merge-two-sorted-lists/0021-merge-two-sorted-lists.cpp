8a934161f1c7a50b6e78525b7e7e72ad3d4fe81b
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-01-09 20:38:57 +0530

    Time: 6 ms (88.27%), Space: 15 MB (13.66%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,66 +10,32 @@
  */
 class Solution {
 public:
-    
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* curr1 = list1;
-        ListNode* curr2 = list2;
         if(list1==NULL)
             return list2;
         if(list2 == NULL)
             return list1;
-        vector<int>arr;
-        while(curr1!=NULL&&curr2!=NULL)
+        ListNode* temp = list1;
+        while(temp->next!=NULL)
         {
-            int d1 = curr1->val;
-            int d2 = curr2->val;
-            if(d1<d2)
-            {
-                arr.push_back(d1);
-                curr1 = curr1->next;
-                
-            }
-            else if(d1==d2)
-            {
-                arr.push_back(d1);
-                arr.push_back(d1);
-                curr1 = curr1->next;
-                curr2 = curr2->next;
-            }
-            else
-            {
-                arr.push_back(d2);
-                curr2 = curr2->next;
-             }
-                
+            temp = temp->next;
         }
-        while(curr1!=NULL)
+        temp->next = list2;
+        vector<int>ml;
+        temp = list1;
+        while(temp!=NULL)
         {
-            arr.push_back(curr1->val);
-            curr1 = curr1->next;
+            ml.push_back(temp->val);
+            temp = temp->next;
         }
-        while(curr2!=NULL)
+        sort(ml.begin(), ml.end());
+        temp = list1;
+        for(int i = 0;i<ml.size();i++)
         {
-            arr.push_back(curr2->val);
-            curr2 = curr2->next;
+            temp->val = ml[i];
+            temp = temp->next;
         }
-        ListNode* head=NULL;
-       
-        ListNode* newnode = new ListNode(arr[0]);
-        head = newnode;
-        ListNode* curr = newnode;
-        for(int i=1;i<arr.size();i++)
-        {
-            ListNode* newnode = new ListNode(arr[i]);
-            curr->next = newnode;
-            curr = newnode;
-            
-        }
-        return head;
-        
-        
-        
-     
+        return list1;
         
     }
 };
