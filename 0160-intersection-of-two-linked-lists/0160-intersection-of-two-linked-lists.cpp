f5577fe572b3765cd39e47f7a3228492a7feb462
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-01-09 19:26:17 +0530

    Time: 54 ms (71.26%), Space: 14.5 MB (89.21%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,22 +8,67 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*>nodeadd;
-        ListNode* temp = headA;
+    int listsize(ListNode* head)
+    {
+        ListNode* temp = head;
+        int size = 0;
         while(temp!=NULL)
         {
-            nodeadd.insert(temp);
+            size++;
             temp = temp->next;
         }
-        temp = headB;
-        while(temp!=NULL)
+        return size;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int len1= listsize(headA);
+        int len2= listsize(headB);
+        cout<<len1<<" "<<len2<<endl;
+        if(len1>len2)
         {
-            if(nodeadd.find(temp)!=nodeadd.end())
-                return temp;
-            temp = temp->next;
+            int d = len1-len2;
+            int count=1;
+            ListNode* temp = headA;
+           for(int i =0;i<d;i++)
+           {
+               if(temp==NULL)
+                   return NULL;
+               temp = temp->next;
+           }
+            cout<<temp->val<<endl;
+            ListNode* temp1 = headB;
+            while(temp!=NULL&&temp1!=NULL)
+            {
+                if(temp==temp1)
+                    return temp;
+                temp = temp->next;
+                temp1 = temp1->next;
+            }
+            return NULL;
+        }
+        else
+        {
+            int d = len2-len1;
+            ListNode* temp = headB;
+            int count = 1;
+            for(int i =0;i<d;i++)
+           {
+               if(temp==NULL)
+                   return NULL;
+               temp = temp->next;
+           }
+            ListNode* temp1 = headA;
+            while(temp1!=NULL&&temp!=NULL)
+            {
+                if(temp1==temp)
+                    return temp1;
+                temp1 = temp1->next;
+                temp = temp->next;
+            }
+            return NULL;
         }
         return NULL;
+        
+        
         
         
     }
