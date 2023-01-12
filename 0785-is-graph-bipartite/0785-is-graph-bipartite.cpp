a5730822cf47191b0afb7a9c9439265c7d6f552f
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-01-12 19:34:43 +0530

    Time: 18 ms (98.63%), Space: 13.4 MB (95.10%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,51 +1,42 @@
 class Solution {
 public:
-    bool bfs(int vertex, vector<int>&color, vector<bool>&visited, vector<vector<int>>&graph)
+    bool dfs(int vertex, vector<int>&color, vector<bool>&visited, vector<vector<int>>&graph)
     {
-       visited[vertex]=true;
-        queue<int>q;
-        q.push(vertex);
-        color[vertex]=1;
+        visited[vertex]=true;
+        bool ans = true;
         
-        while(q.size()!=0)
+        for(int i = 0;i<graph[vertex].size();i++)
         {
-            int currnode = q.front();
-            q.pop();
-            for(int i=0;i<graph[currnode].size();i++)
+            int adjnode = graph[vertex][i];
+            if(visited[adjnode]==false)
             {
-                int adjnode = graph[currnode][i];
-                if(visited[adjnode]==true)
-                {
-                    if(color[adjnode]!=-1&&(color[currnode]==color[adjnode]))
-                        return false;
-                }
-                else
-                {
-                    color[currnode]==1?color[adjnode]=0:color[adjnode]=1;
-                    visited[adjnode]=true;
-                    q.push(adjnode);
-                }
+                color[vertex]==1?color[adjnode]=0:color[adjnode]=1;
+                ans = dfs(adjnode, color, visited, graph);
             }
+            else
+            {
+                if(color[adjnode]!=-1&&color[adjnode]==color[vertex])
+                    return false;
+            }
+            
         }
-        return true;
+        return ans;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<bool>visited(V, false);
+       int V = graph.size(); 
+       vector<bool>visited(V, false);
         vector<int>color(V, -1);
-        bool ans = false;
         for(int i =0;i<V;i++)
         {
             if(visited[i]==false)
             {
-                bool temp = bfs(i, color, visited, graph);
+                color[i]=1;
+                bool temp = dfs(i, color, visited, graph);
                 if(temp==false)
                     return false;
             }
         }
         return true;
-        
-        
         
     }
 };
