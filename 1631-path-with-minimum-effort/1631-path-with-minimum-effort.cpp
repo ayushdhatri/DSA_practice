c2f27b7efc0977740a8dadb540a8280322c32a22
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-02-08 18:54:34 +0530

    Time: 175 ms (59.47%), Space: 20 MB (84.50%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,41 +3,38 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
+        priority_queue<pair<int,pair<int, int>>,
+        vector<pair<int, pair<int, int>>>,
+        greater<pair<int,pair<int, int>>>>pq;
         vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
         dist[0][0]=0;
-        priority_queue<pair<int, pair<int, int>>,
-        vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>pq;
         pq.push({0,{0,0}});
-        int dr[]={-1,0,+1,0};
-        int dc[]={0,+1,0,-1};
+        int drow[]={-1,0,+1,0};
+        int dcol[]={0,+1,0,-1};
         while(pq.size()!=0)
         {
-            int pdis = pq.top().first;
             int prow = pq.top().second.first;
             int pcol = pq.top().second.second;
+            int pdis = pq.top().first;
             pq.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow = prow+dr[i];
-                int ncol = pcol+dc[i];
+                int nrow = prow+drow[i];
+                int ncol = pcol+dcol[i];
                 if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m)
                 {
-                    int neweffort = max(abs(heights[nrow][ncol]-heights[prow][pcol]),dist[prow][pcol]);
+                    int neweffort = max(pdis, abs(heights[nrow][ncol]-heights[prow][pcol]));
                     if(neweffort<dist[nrow][ncol])
                     {
-                        dist[nrow][ncol]= neweffort;
-                        pq.push({neweffort,{nrow ,ncol}});
+                        dist[nrow][ncol]=neweffort;
+                        pq.push({neweffort,{nrow, ncol}});
                     }
-                    
                 }
-                
+
             }
-            
-            
         }
         return dist[n-1][m-1];
-            
+        
         
     }
 };
