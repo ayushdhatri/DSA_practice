07140f18037a10335702daa1f01244fde7b26779
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-02-08 11:59:08 +0530

    Time: 158 ms (30.79%), Space: 45.2 MB (28.60%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int mincoins(vector<int>&coins, int amount, int index, vector<vector<int>>&dp)
-    {
-        if(amount==0)
-            return 0;
-            
-        if(index==0)
-        {
-            if(amount%coins[0]==0)
-                return amount/coins[0];
-            return 1e5;
-        }
-        if(dp[index][amount]!=-1)
-            return dp[index][amount];
-        int pick = 1e5;
-        if(amount>=coins[index])
-          pick=1+ mincoins(coins,amount-coins[index], index,dp);
-        int notpick = 0 + mincoins(coins, amount, index-1,dp);
-        return dp[index][amount] = min(pick, notpick);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1, -1));
-        int temp = mincoins(coins, amount,n-1,dp);
-        if(temp>=1e5)
-            return -1;
-        return temp;
+        vector<vector<int>>dp(n, vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+                dp[0][i]=i/coins[0];
+            else
+                dp[0][i]=1e9;
+        }
+        for(int row=1;row<n;row++)
+        {
+            for(int target = 0;target<=amount;target++)
+            {
+                int notinclude = dp[row-1][target];
+                int include = 1e9;
+                if(target>=coins[row]) {
+                    include = 1+dp[row][target-coins[row]];
+                }
+                dp[row][target]=min(notinclude,include);
+            }
+        }
+        if(dp[n-1][amount]!=1e9)
+        return dp[n-1][amount];
+        return -1;
+        
+        
+        
         
     }
 };
