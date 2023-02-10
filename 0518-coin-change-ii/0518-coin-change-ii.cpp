e4252f3e2db67c16cd6df5592fa0150ac6c21f7b
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-02-10 19:18:14 +0530

    Time: 9 ms (90.59%), Space: 7.2 MB (84.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int totalways(vector<int>&coins, int amount,int index, vector<vector<int>>&dp)
-    {
-        if(amount==0)
-            return 1;
-        if(index==0)
-        {
-            if(amount%coins[index]==0)
-                return 1;
-            return 0;
-        }
-        if(dp[index][amount]!=-1)
-            return dp[index][amount];
-        int pick =0;
-        if(amount>=coins[index])
-            pick = totalways(coins, amount-coins[index], index,dp);
-        int notpick = totalways(coins, amount, index-1,dp);
-        return dp[index][amount] = pick + notpick;
-    }
     int change(int amount, vector<int>& coins) {
+        vector<int>dp(amount+1, 0);
+        dp[0]=1;
+        for(int i=0;i<coins.size();i++)
+        {
+            int value = coins[i];
+            for(int target=1;target<=amount;target++)
+            {
+                if(target>=value)
+                {
+                    dp[target]+=dp[target-value];
+                }
+                
+            }
+        }
         
-        int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
-        return totalways(coins, amount, n-1, dp);
+        return dp[amount];
     }
 };
