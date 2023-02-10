cd921e27182c6d3b1f0bc2257e25490954c1a275
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-02-10 21:36:51 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,54 +7,29 @@ using namespace std;
 class Solution
 {
     public:
-        int maxpossible(int index, int wt[], int val[], int total, vector<vector<int>>&dp)
-        {
-            if(total==0)
-            {
-                return 0;
-            }
-            if(index==0)
-            {
-              if(total>=wt[0])
-               return val[0];
-               return 0;
-            }
-            if(dp[index][total]!=-1)
-                return dp[index][total];
-            
-            int pick = 0;
-            if(total>=wt[index])
-                pick = val[index] + maxpossible(index-1, wt, val, total-wt[index], dp);
-            int notpick = maxpossible(index-1, wt, val, total,dp);
-            return dp[index][total]=max(pick, notpick);
-        }
-    
+    //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>>dp(n, vector<int>(W+1, 0));
-       for(int i=0;i<n;i++)
+       vector<vector<int>>dp(n,vector<int>(W+1, 0));
+       for(int i=0;i<=W;i++)
        {
-           dp[i][0]=0;
+           if(i>=wt[0])
+           dp[0][i]=val[0];
        }
-       for(int index=wt[0];index<=W;index++)
-        dp[0][index]=val[0];
-       if(wt[0]<=W)
-            dp[0][wt[0]]=val[0];
-        for(int i=1;i<n;i++)
-        {
-            for(int j =1;j<W+1;j++)
-            {
-                int pick = 0;
-                if(j>=wt[i])
-                    pick = val[i]+dp[i-1][j-wt[i]];
-                int notpick = dp[i-1][j];
-                dp[i][j]=max(pick, notpick);
-            }
-        }
-        return dp[n-1][W];
-       
-       //return maxpossible(n-1, wt, val, W,dp);
+       for(int i=0;i<n;i++) dp[i][0]=0;
+       for(int i=1;i<n;i++)
+       {
+           for(int ksack = 1;ksack<=W;ksack++)
+           {
+               int notinclude = dp[i-1][ksack];
+               int include = 0;
+               if(ksack>=wt[i])
+                    include = val[i]+dp[i-1][ksack-wt[i]];
+               dp[i][ksack]=max(include,notinclude);
+           }
+       }
+       return dp[n-1][W];
        
     }
 };
