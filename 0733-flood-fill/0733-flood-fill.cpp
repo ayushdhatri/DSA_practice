033762ba8874c3f226f700b9745487f16746ca90
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-03-16 17:37:33 +0530

    Time: 15 ms (22.61%), Space: 14.5 MB (13.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,19 +3,20 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size();
         int n = image[0].size();
-        vector<vector<bool>>visited(m, vector<bool>(n, false));
+        int ic = image[sr][sc];
         queue<pair<int, int>>q;
         q.push({sr, sc});
-        int ic = image[sr][sc];
-        image[sr][sc]=color;
+        vector<vector<bool>>visited(m, vector<bool>(n,false));
         int drow[]={-1,0,+1,0};
         int dcol[]={0,+1,0,-1};
+        visited[sr][sc]=true;
+        image[sr][sc]=color;
         while(q.size()!=0)
         {
             int prow = q.front().first;
             int pcol = q.front().second;
             q.pop();
-            for(int d = 0;d<4;d++)
+            for(int d=0;d<4;d++)
             {
                 int nrow = prow+drow[d];
                 int ncol = pcol+dcol[d];
@@ -24,10 +25,14 @@ public:
                     visited[nrow][ncol]=true;
                     image[nrow][ncol]=color;
                     q.push({nrow, ncol});
+                    
                 }
-            }    
+            }
+            
+            
         }
         return image;
+        
         
     }
 };
