fe5e1189b6c1dfd4ac8d44e7d2b92f3138ad2a0b
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-05-02 13:55:04 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -11,25 +11,28 @@ class Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>>dp(n,vector<int>(W+1, 0));
-       for(int i=0;i<=W;i++)
-       {
-           if(i>=wt[0])
-           dp[0][i]=val[0];
-       }
-       for(int i=0;i<n;i++) dp[i][0]=0;
-       for(int i=1;i<n;i++)
-       {
-           for(int ksack = 1;ksack<=W;ksack++)
-           {
-               int notinclude = dp[i-1][ksack];
-               int include = 0;
-               if(ksack>=wt[i])
-                    include = val[i]+dp[i-1][ksack-wt[i]];
-               dp[i][ksack]=max(include,notinclude);
-           }
-       }
-       return dp[n-1][W];
+       vector<vector<int>>dp(n+1, vector<int>(W+1,0));
+       
+       for(int row = 0;row<=n;row++) dp[row][0]=0;
+       for(int col = 0;col<=W;col++)dp[0][col]=0;
+       
+      for(int row=1;row<=n;row++)
+      {
+          for(int col = 1;col<=W;col++)
+          {
+              int exc = dp[row-1][col];
+              int inc = INT_MIN;
+              if(col>=wt[row-1])
+              {
+                  inc = val[row-1]+dp[row-1][col-wt[row-1]];
+              }
+              dp[row][col]=max(exc, inc);
+               
+          }
+      }
+       return dp[n][W];
+       
+       
        
     }
 };
