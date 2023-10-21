9d461a246d733a0fb01ede8fd5a991bc876279de
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-10-21 18:29:31 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,39 +6,37 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(int vertex, vector<bool>&visited,vector<bool>&pathvisited ,vector<int> adj[])
+    bool dfs(int index,int parent ,int V, vector<int>adj[], vector<bool>&visited, vector<bool>&path_visited)
     {
-        visited[vertex]=true;
-        pathvisited[vertex]=true;
-        bool ans = false;
-        for(auto x:adj[vertex])
-        {
-           if(visited[x]==false)
-           {
-               if(dfs(x, visited, pathvisited, adj)==true)
-                return true;
-           }
-           else if(pathvisited[x])
-            return true;
+        visited[index]=true;
+        path_visited[index]=true;
+        for(auto x: adj[index]){
+            if(visited[x]==true)
+            {
+                if(path_visited[x]==true)return true;
+                
+            }
+            else{
+                bool temp = dfs(x, index, V, adj, visited, path_visited);
+                if(temp==true)return true;
+            }
         }
-        pathvisited[vertex]=false;
-        return ans;
+        path_visited[index]=false;
+        return false;
     }
+    
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
         vector<bool>visited(V, false);
-        vector<bool>pathvisited(V, false);
-        for(int i=0;i<V;i++)
-        {
-            if(visited[i]==false)
-            {
-                bool temp = dfs(i, visited,pathvisited,adj);
-                if(temp==true)
-                return true;
-                
+        vector<bool>path_visited(V, false);
+        bool flag = false;
+        for(int i = 0;i<V;i++){
+            if(visited[i]==false){
+                bool temp = dfs(i, -1,V, adj, visited, path_visited);
+                flag = flag||temp;
             }
         }
-        return false;
+        return flag;
     }
 };
 
