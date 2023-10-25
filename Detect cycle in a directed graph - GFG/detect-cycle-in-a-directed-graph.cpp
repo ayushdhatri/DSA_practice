df511f500f7aa87c5af5a2ad860088261d0f37f9
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-10-25 21:33:09 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,37 +6,35 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(int index,int parent ,int V, vector<int>adj[], vector<bool>&visited, vector<bool>&path_visited)
-    {
-        visited[index]=true;
-        path_visited[index]=true;
-        for(auto x: adj[index]){
-            if(visited[x]==true)
-            {
+    bool dfs(int vertex, vector<int>adj[], vector<bool>&visited, vector<bool>&path_visited){
+        visited[vertex]=true;
+        path_visited[vertex]=true;
+        for(auto x: adj[vertex])
+        {
+            if(visited[x]==true){
                 if(path_visited[x]==true)return true;
-                
             }
             else{
-                bool temp = dfs(x, index, V, adj, visited, path_visited);
+                bool temp = dfs(x, adj, visited, path_visited);
                 if(temp==true)return true;
             }
         }
-        path_visited[index]=false;
+        path_visited[vertex]=false;
         return false;
     }
-    
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
         vector<bool>visited(V, false);
         vector<bool>path_visited(V, false);
-        bool flag = false;
-        for(int i = 0;i<V;i++){
+        for(int i= 0;i<V;i++){
             if(visited[i]==false){
-                bool temp = dfs(i, -1,V, adj, visited, path_visited);
-                flag = flag||temp;
+                bool temp = dfs(i, adj, visited, path_visited);
+                if(temp==true)return true;
             }
         }
-        return flag;
+        return false;
+        
+        
     }
 };
 
