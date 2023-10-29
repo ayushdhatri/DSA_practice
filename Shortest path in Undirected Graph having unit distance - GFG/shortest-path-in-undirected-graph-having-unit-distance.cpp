5fd2c75ea91dd3bd25f516bf3d2467a3d479e91a
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2023-10-29 13:00:54 +0530

    Added solution - LeetHub

--- Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.cpp
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.cpp
@@ -8,42 +8,39 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-  void dfs(int vertex, vector<int>&dist, vector<bool>&visited, vector<int>adj[]){
-      visited[vertex]=true;
-      for(auto x: adj[vertex])
-      {
-          if(!visited[x])
-          {
-              if(dist[vertex]+1<dist[x])
-                dist[x]=dist[vertex]+1;
-            dfs(x, dist, visited, adj);    
-          }
-      }
-      visited[vertex]=false;
-  }
     vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
         // code here
+        vector<int>dist(N, INT_MAX);
         vector<int>adj[N];
-        for(int i=0;i<M;i++)
-        {
+        for(int i = 0;i<M;i++){
             int u = edges[i][0];
             int v = edges[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<int>dist(N, INT_MAX);
         vector<bool>visited(N, false);
+        queue<int>q;
+        q.push(src);
+        visited[src]=true;
         dist[src]=0;
-        dfs(src, dist, visited, adj);
-        for(int i=0;i<N;i++)
-        {
-            if(dist[i]==INT_MAX)
-            {
-                dist[i]=-1;
+        while(q.size()!=0){
+            int curr_node = q.front();
+            q.pop();
+            for(auto adj_node: adj[curr_node]){
+                if(visited[adj_node]==false){
+                    visited[adj_node]=true;
+                    if(dist[curr_node]+1<dist[adj_node]){
+                        dist[adj_node]=dist[curr_node]+1;
+                    }
+                    q.push(adj_node);
+                }
+                
             }
         }
-       
-       return dist;
+        for(int i = 0;i<N;i++){
+            if(dist[i]==INT_MAX)dist[i]=-1;
+        }
+        return dist;
     }
 };
 
