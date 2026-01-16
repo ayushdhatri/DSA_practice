cd617fb85892826fe646eb6c1bf7d5f6104f6d09
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-01-16 20:53:50 +0530

    Time: 537 ms (5.09%), Space: 21.5 MB (41.58%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,56 +1,45 @@
 class Solution {
 public:
-    int totalways(int row,vector<vector<int>>&grid, int lastindex, vector<vector<int>>&dp)
-    {
-        if(row==grid.size())
-            return 0;
-        if(dp[row][lastindex]!=-1)
-            return dp[row][lastindex];
-        int ans = INT_MAX;
-        for(int i=0;i<grid.size();i++)
-        {
-            if(i==lastindex)continue;
-            int temp = grid[row][i]+totalways(row+1,grid,i,dp);
-            ans  = min(ans, temp);
-        }
-        return dp[row][lastindex]=ans;
+int minPathSum(int currRow, int currCol, vector<vector<int>>&grid, vector<vector<int>>&dp){
+    // pruning
+    if(currRow < 0 || currRow >=grid.size() || currCol < 0 || currCol >=grid[0].size()){
+        return INT_MAX;
     }
+
+    // base case
+    if(currRow == grid.size()-1){
+        return grid[currRow][currCol];
+    }
+
+    // cache
+    if(dp[currRow][currCol] != 1e9){
+        return dp[currRow][currCol];
+    }
+
+    // transition
+    int ans = 1e9;
+    for(int i = 0;i<grid[0].size();i++){
+        if(currCol != i){
+            ans = min(ans, grid[currRow][currCol] + minPathSum(currRow + 1, i, grid, dp));
+        }
+    }
+
+    // save and return
+    return dp[currRow][currCol] = ans;
+
+}
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
-        
-        vector<vector<int>>dp(n, vector<int>(n, 0));
-//         for(int i=0;i<n;i++){
-//             int temp = grid[0][i]+totalways(1,grid,i,dp);
-//             ans = min(temp, ans);
-//         }
-//         return ans;
-        for(int i=0;i<n;i++)
-        {
-            dp[0][i]=grid[0][i];
+        int m = grid[0].size();
+        vector<vector<int>>dp(n, vector<int>(n,1e9));
+        int ans = 1e9;
+        for(int i = 0;i<n;i++){
+            ans = min(ans, minPathSum(0,i,grid, dp));
         }
-       
-            for(int i=1;i<n;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    int ans = INT_MAX;
-                    int temp;
-                    for(int base=0;base<n;base++)
-                    {
-                        if(base==j)continue;
-                        temp = grid[i][j]+dp[i-1][base];
-                        ans=min(ans, temp);
-                    }
-                    dp[i][j]=ans;
-                }
-            }
-        int fs= INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            fs= min(fs, dp[n-1][i]);
-        }
-        return fs;
+        return ans;
+
+
+
         
-      
     }
 };
