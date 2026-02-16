3bdb1ac0a9460f48b0ef79a599bbdf04df5228b1
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-02-16 21:00:02 +0530

    Time: 20 ms (9.08%), Space: 16.4 MB (99.99%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,44 +1,54 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int, int>> adj[n];
+
+        int cheapFlightPrice = INT_MAX;
+        vector<pair<int, int>>adj[n];
         int m = flights.size();
-        for(int i=0;i<m;i++)
-        {
+        for(int i= 0;i<m;i++){
             int u = flights[i][0];
             int v = flights[i][1];
-            int wt = flights[i][2];
-            adj[u].push_back({v,wt});
+            int price = flights[i][2];
+            adj[u].push_back({v, price});
         }
-        vector<int>dist(n, INT_MAX);
-        dist[src]=0;
-        priority_queue<pair<int, pair<int, int>>,
-        vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>pq;
-        pq.push({0,{src,0}});
-        while(pq.size()!=0)
-        {
-            int pstop = pq.top().first;
-            int pnode = pq.top().second.first;
-            int pdis = pq.top().second.second;
+        vector<int>price(n, 1e9);
+        priority_queue<pair<int, pair< int, int>>, 
+        vector<pair< int, pair< int, int>>>, 
+        greater<pair< int, pair< int, int>>>
+        >pq;
+        price[src]=0;
+        pq.push({0,{src, 0}});
+        while(pq.size()!=0){
+            int currStops = pq.top().first;
+            int currNode = pq.top().second.first;
+            int currPrice = pq.top().second.second;
             pq.pop();
-            for(auto x: adj[pnode])
-            {
-                int adjnode = x.first;
-                int adjdis = x.second;
-                if(pstop>k)continue;
-                if(pdis+adjdis<dist[adjnode])
-                {
-                    dist[adjnode]=pdis+adjdis;
-                    pq.push({pstop+1, {adjnode, dist[adjnode]}});
+            for(auto x: adj[currNode]){
+                int adjNode = x.first;
+                int adjPrice = x.second;
+                if(currStops+1<=(k+1)){
+                    if(currPrice+adjPrice<price[adjNode]){
+                        price[adjNode]= currPrice + adjPrice;
+                        pq.push({currStops+1,{adjNode, price[adjNode]}});
+                    }
                 }
             }
-        } 
-        if(dist[dst]==INT_MAX)return -1;
-        return dist[dst];
-        
-        
-        
+            
+        }
+        if(price[dst]==1e9)return -1;
+        return price[dst];
+        return -1;
+    
+
+
+
+
+
+
+
+
+
+        return cheapFlightPrice;
         
     }
 };
