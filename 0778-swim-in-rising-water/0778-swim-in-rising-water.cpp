d589cfdfb4c12de2c50fecccb40f29c31a73f750
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-02-17 20:37:07 +0530

    Time: 7 ms (65.04%), Space: 12.8 MB (77%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -3,41 +3,35 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        priority_queue<pair<int, pair<int, int>>,
-        vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>pq;
-        vector<vector<int>>time(n, vector<int>(m, INT_MAX));
+        vector<vector<int>>dist(n+1,vector<int>(m+1,1e9));
+        priority_queue<pair<int,pair<int, int>>,
+        vector<pair<int,pair<int, int>>>,
+        greater<pair<int,pair<int, int>>>
+        >pq;
+        dist[0][0] = grid[0][0];
         pq.push({grid[0][0],{0,0}});
-        time[0][0]=grid[0][0];
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
-        while(pq.size()!=0)
-        {
-            int prow = pq.top().second.first;
-            int pcol =pq.top().second.second;
-            int ptime = pq.top().first;
+        int drow[] = {-1,0,+1,0};
+        int dcol[] = {0,+1,0,-1};
+        while(pq.size()!=0){
+            int currRow = pq.top().second.first;
+            int currCol = pq.top().second.second;
+            int currEle = pq.top().first;
             pq.pop();
-            for(int i=0;i<4;i++)
-            {
-                int nrow = prow+drow[i];
-                int ncol= pcol+dcol[i];
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<n)
-                {
-                    if(grid[nrow][ncol]>=ptime&&grid[nrow][ncol]<time[nrow][ncol])
-                    {
-                        time[nrow][ncol]=grid[nrow][ncol];
-                        pq.push({grid[nrow][ncol], {nrow,ncol}});
-                    }
-                    else if(grid[nrow][ncol]<ptime&&ptime<time[nrow][ncol])
-                    {
-                        time[nrow][ncol]=ptime;
-                        pq.push({ptime, {nrow, ncol}});
-                        
+
+            for(int i = 0;i<4;i++){
+                int nrow = currRow + drow[i];
+                int ncol = currCol + dcol[i];
+                if(nrow >=0 && nrow<n && ncol >=0 && ncol<m){
+                    if(max(grid[nrow][ncol],currEle) < dist[nrow][ncol]){
+                        dist[nrow][ncol] = max(grid[nrow][ncol],currEle);
+                        pq.push({dist[nrow][ncol],{nrow,ncol}});
                     }
                 }
             }
         }
-        return time[n-1][m-1];
-         
+        return dist[n-1][m-1];
+
+
+        
     }
 };
