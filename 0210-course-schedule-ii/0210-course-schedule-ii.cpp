5b3999386f75d0af6ca2c19471d6b175ef230c5f
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-02-23 21:59:36 +0530

    Time: 5 ms (44.26%), Space: 18.1 MB (83.71%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>adj[numCourses];
-        vector<int>ans;
-        for(int i=0;i<prerequisites.size();i++)
-        {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        int m = prerequisites.size();
+        vector<vector<int>>graph(numCourses);
+        for(int i = 0;i<m;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            graph[v].push_back(u);
+        }
+        vector<int>indegree(numCourses, 0);
+        for(int i = 0;i<numCourses;i++){
+            for(int j = 0;j<graph[i].size();j++){
+                indegree[graph[i][j]]+=1;
+            }
         }
         queue<int>q;
-        vector<int>indegree(numCourses, 0);
-        for(int i=0;i<numCourses;i++)
-        {
-            for(auto x: adj[i])
-            {
-                indegree[x]++;
-            }
-
-        }
-        for(int i = 0;i<numCourses;i++)
-        {
-            if(indegree[i]==0)
+        vector<int>topo;
+        for(int i = 0;i<numCourses;i++){
+            if(indegree[i] == 0){
                 q.push(i);
-        }
-        while(q.size()!=0)
-        {
-            int currnode = q.front();
-            q.pop();
-            ans.push_back(currnode);
-            for(auto x: adj[currnode])
-            {
-                indegree[x]--;
-                if(indegree[x]==0)
-                    q.push(x);
-
             }
         }
-        if(ans.size()==numCourses)return ans;
-        vector<int>temp;
-        return temp;
-
-        
+        while(q.size()!= 0){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto x: graph[node]){
+                indegree[x]--;
+                if(indegree[x] == 0)q.push(x);
+            }
+        }
+        if(topo.size() == numCourses)return topo;
+        return {};
     }
 };
