ba35fdc0c767c839e1a2f2ef4e7e0337ec19422f
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-02-23 21:49:18 +0530

    Time: 3 ms (77.06%), Space: 19.4 MB (55.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int ans=0;
-        vector<int>indegree(numCourses, 0);
-        vector<int> adj[numCourses];
-        int vertex = prerequisites.size();
-        for(int i = 0;i<vertex;i++)
-        {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        int m = prerequisites.size();
+        vector<vector<int>>adj(numCourses);
+        for(int i = 0;i<m;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        // first construct the indegree
+        vector<int>indegree(numCourses,0);
+        for(int i = 0;i<numCourses;i++){
+            for(int j = 0;j<adj[i].size();j++){
+                indegree[adj[i][j]]+=1;
+            }
         }
         queue<int>q;
-        for(int i = 0;i<numCourses;i++)
-        {
-            for(auto x: adj[i])
-            {
-                indegree[x]++;
-            }
+        for(int i = 0;i<numCourses;i++){
+            if(indegree[i] == 0) q.push(i);
         }
-        for(int i = 0;i<numCourses;i++)
-        {
-            if(indegree[i]==0)
-            {
-                q.push(i);
-            }
-        }
-        while(q.size()!=0)
-        {
-            int currvertex = q.front();
+        // take queue
+        vector<int>topo;
+        while(q.size()!=0){
+            int node = q.front();
             q.pop();
-            ans++;
-            for(auto x: adj[currvertex])
-            {
+            topo.push_back(node);
+            for(auto x: adj[node]){
                 indegree[x]--;
-                if(indegree[x]==0)
+                if(indegree[x] == 0){
                     q.push(x);
+                }
             }
 
         }
-        if(ans==numCourses)return true;
-        return false;
+
+
+        return topo.size() == numCourses;
+
+
+        
+
+
+
         
     }
 };
