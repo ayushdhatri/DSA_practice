eebef058c2397b3f81f16da09f2da00f5e1d2dd7
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-04-26 11:32:31 +0530

    Time: 0 ms (100%), Space: 134.2 MB (83.71%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,25 +1,29 @@
+#define ll long long
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
+        if(k == 0) return nums;
         int n = nums.size();
-        vector<long long> prefixSum(n, 0);
-        prefixSum[0] = nums[0];
-        for(int i = 1;i<n;i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
-        vector<int>ans(n, -1);
-        if(n <= k)return ans;
-        for(int i = k;i<(n-k);i++){
-            int left = i -k;
-            int right = i + k;
-            if(left == right){
-                ans[i] = nums[i];
+        vector<int> ans(n, -1);
+        int window_len = 2*k+1;
+        int tail = 0, head = 0;
+        ll sum = 0;
+        while(head < n){
+            if(head - tail + 1 < window_len){
+                sum+=nums[head];
+                head+=1;
             }
-            else{
-                ans[i] = (left == 0) ? (prefixSum[right])/(2*k + 1) : (prefixSum[right] - prefixSum[left-1])/(2*k + 1);
+            else if(head - tail + 1 == window_len){
+                sum+=nums[head];
+                ans[tail + k] = (sum/(1LL*window_len));
+                sum-=nums[tail];
+                tail+=1;
+                head+=1;
             }
         }
-        return ans;
         
+
+
+        return ans;
     }
 };
