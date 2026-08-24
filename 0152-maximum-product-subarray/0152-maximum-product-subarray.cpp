a310f9f4cf8098c1438f3954b6ce8254e063304e
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-08-24 23:22:29 +0530

    Time: 0 ms (100%), Space: 18.5 MB (5.67%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,27 +1,19 @@
-const int INF = 1e9;
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // I will define the maeaning  : dp[i] maxium product of subarray ending at i
+        // we can have dp[i] = maximum subarray ending at i
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(2, -INF));
-        dp[0][0] = nums[0];
-        dp[0][1] = nums[0];
-
+        vector<int>dp1(n, 1);
+        vector<int>dp2(n, 1);
+        dp1[0] = nums[0];
+        dp2[0] = nums[0];
+        int ans = nums[0];
         for(int i = 1;i<n;i++){
-           int curr = nums[i];
-           int currMax = nums[i] * dp[i-1][0];
-           int currMin = nums[i] * dp[i-1][1];
-           dp[i][0] = max({curr, currMax, currMin});
-           dp[i][1] = min({curr, currMax, currMin});
-        }
-        int ans = -INF;
-        for(int i = 0;i<n;i++){
-            cout<<dp[i][0]<<" "<<dp[i][1]<<endl;
-            ans = max({ans,dp[i][0],dp[i][1]});
+            dp1[i] = max({nums[i], nums[i] * dp1[i-1], nums[i] * dp2[i-1]});
+            dp2[i] = min({nums[i], nums[i] * dp1[i-1], nums[i] * dp2[i-1]});
+            ans = max(ans, dp1[i]);
+         
         }
         return ans;
-
-        
     }
 };
