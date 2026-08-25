303e614bd9d15112dcf2bb3ab7e5c2404bfeb793
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-08-25 21:06:34 +0530

    Time: 0 ms (100%), Space: 11 MB (7.4%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    int houserob(vector<int>&nums)
-    {
-        int n = nums.size();
-        vector<int>dp(n, 0);
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++){
-            int notpick = 0 + dp[i-1];
-            int pick  = nums[i];
-            if(i-2>=0)
-            {
-                pick+=dp[i-2];
-            }
-            dp[i]=max(pick, notpick);
-        }
-        return dp[n-1];
-        
-    }
+int dp[110];
+int rec(int level, vector<int>&nums){
+    // pruning
+    if(level >= nums.size())return 0;
+
+    // base case
+
+
+    // cache
+    if(dp[level] != -1)return dp[level];
+    // transition
+    int take = nums[level] + rec(level + 2, nums);
+    int dontTake = rec(level + 1, nums);
+
+    // save and return
+    return dp[level] = max(take, dontTake);
+}
     int rob(vector<int>& nums) {
-        if(nums.size()==1)
-            return nums[0];
         int n = nums.size();
-        vector<int>nums1(n-1);
-        for(int i=0;i<n-1;i++)nums1[i]=nums[i];
-        vector<int>nums2(n-1);
-        for(int i=1;i<n;i++)nums2[i-1]=nums[i];
-        int ans1 = houserob(nums1);
-        int ans2 = houserob(nums2);
-        return max(ans1, ans2);
-        
-        
+        if(n == 1)return nums[0];
+        vector<int>arr1(n-1), arr2(n-1);
+        int m = n-1;
+        for(int i = 0;i<m;i++){
+            arr1[i] = nums[i];
+        }
+        // cout<<"Size of arr2 : "<<arr2.size()<<endl;
+        for(int i = 0;i<m;i++){
+            arr2[i] = nums[i+1];
+        }
+        memset(dp, -1, sizeof(dp));
+        int max1 = rec(0,arr1);
+        memset(dp, -1, sizeof(dp));
+        int max2 = rec(0, arr2);
+        return max(max1, max2);
         
     }
 };
