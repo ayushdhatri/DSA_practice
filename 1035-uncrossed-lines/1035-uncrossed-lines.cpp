205b46abd35e0addca2f281ab7f6af6de9094c8d
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-09-07 16:06:07 +0530

    Time: 23 ms (6.63%), Space: 15.1 MB (81.14%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-int maxConnectLine(int index1, int index2, vector<int>&nums1, vector<int>&nums2, vector<vector<int>>&dp){
+int dp[501][501];
+int rec(int level1, int level2, vector<int>&nums1, vector<int>&nums2){
     // pruning
-    
-    // base case
-    if(index1 >= nums1.size() || index2 >= nums2.size())return 0;
 
+    // base case
+    if(level1 >= nums1.size() || level2 >= nums2.size())return 0;
     // cache
-    if(dp[index1][index2] != -1)return dp[index1][index2];
+    if(dp[level1][level2] != -1)return dp[level1][level2];
+
     // transition
     int ans = 0;
-    if(nums1[index1] == nums2[index2]){
-        ans = 1 + maxConnectLine(index1 + 1, index2 + 1, nums1, nums2, dp);
+    if(nums1[level1] == nums2[level2]){
+        ans = max(ans, 1 + rec(level1 + 1, level2 + 1, nums1, nums2));
     }
     else{
-        ans = max(maxConnectLine(index1 + 1, index2, nums1, nums2, dp),
-        maxConnectLine(index1, index2 + 1, nums1, nums2, dp));
+        int way1 = rec(level1 + 1, level2, nums1, nums2);
+        int way2 = rec(level1, level2 + 1, nums1, nums2);
+        ans = max({ans, way1, way2});
     }
 
-
     // save and return
-    return dp[index1][index2] = ans;
-
-
-
+    return dp[level1][level2] = ans;
 }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return maxConnectLine(0,0,nums1,nums2,dp);
+        // we need to find maximum number of connecting lines
+        // which means we need to maximize the connecting line
+        // connecting lines should not intersect
+        // a number in nums1 can match with a number in nums2
+        // when we can have a intersecting line?
+        // 1 4 2
+        // 1 2 4 2
+        //  both of them should have common elements and it should be in sequence
+        // we need to find the lcs of two string
+        memset(dp, -1, sizeof(dp));
+        return rec(0,0,nums1, nums2);
         
     }
 };
