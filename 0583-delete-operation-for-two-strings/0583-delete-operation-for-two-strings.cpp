c871d12905fa1cc430eb802cf8cb68e3303a6b5c
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-09-07 16:22:24 +0530

    Time: 66 ms (5.99%), Space: 12.3 MB (81.88%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-int minDelString(int index1, int index2, string &word1, string &word2, vector<vector<int>>&dp){
-    // pruning case
-    if(index1 == word1.size()){
-        return word2.size() - index2;
-    }
-    if(index2 == word2.size()){
-        return word1.size() - index1;
-    }
+int dp[ 510][510];
+int rec(int level1, int level2, string &word1, string &word2){
+    // pruing
 
     // base case
-
+    if(level1 >= word1.size() || level2 >= word2.size())return 0;
 
     // cache
-    if(dp[index1][index2] != -1){
-        return dp[index1][index2];
-    }
+    if(dp[level1][level2] != -1)return dp[level1][level2];
 
     // transition
-    int ans = INT_MAX;
-    if(word1[index1] == word2[index2]){
-        ans = min(ans, minDelString(index1 + 1, index2 + 1, word1, word2, dp));
+    int ans = 0;
+    if(word1[level1] == word2[level2]){
+        ans = max(ans, 1 + rec(level1 + 1, level2 + 1, word1, word2));
     }
     else{
-        // if i delete charcter in frist string
-        ans = min(ans, 1 + minDelString(index1 + 1, index2, word1, word2, dp));
-        ans = min(ans, 1 + minDelString(index1, index2 + 1, word1, word2, dp));
-
+        ans = max({ans, rec(level1 + 1, level2, word1, word2), rec(level1, level2 + 1, word1, word2)});
     }
 
-
     // save and return
-    return dp[index1][index2] = ans;
-
+    return dp[level1][level2] = ans;
 }
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>>dp(n, vector<int>(m,-1));
-        return minDelString(0,0,word1, word2,dp);
+       //  if word1, and word2 are same then no steps required return 0
+       // the only step we can take is to delete the characters from either string 
+       // sea and eat 
+       // le etco de and etco
+       // if we find the common characters in sequence and remove from both the string what remains in each of them is supposed 
+       // to be deleted
+       memset(dp, -1, sizeof(dp));
+       int lcs = rec(0,0,word1,word2);
+       return ((word1.size() - lcs) + (word2.size() - lcs));
 
-        
     }
 };
