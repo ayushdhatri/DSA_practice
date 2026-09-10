5ff1694be8e54b33098937e8cb3501f54cfd4fa8
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-09-10 17:10:45 +0530

    Time: 36 ms (25.83%), Space: 13 MB (36.47%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    
-    bool palindromSubstr(int i, int j, string &s){
-        if(i >= j) return true;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        bool ans = false;
-        if(s[i] == s[j]){
-            ans = palindromSubstr(i+1, j-1, s);
-        }
-        return dp[i][j] = ans;
+int dp[1010][1010];
+bool rec(int startIndex, int endIndex, string &s){
+    // pruning
+
+    // base case
+    if(startIndex >= endIndex)return true;
+    // cache
+    if(dp[startIndex][endIndex] != -1 )return dp[startIndex][endIndex];
+
+    // transition
+    bool isPalindrome = 0;
+    if(s[startIndex] == s[endIndex]){
+        isPalindrome = rec(startIndex + 1,  endIndex - 1, s);
     }
-    
+
+    // save and return
+    return dp[startIndex][endIndex] = isPalindrome;
+}
     int countSubstrings(string s) {
         int n = s.size();
-        memset(dp, -1, sizeof(dp));
-        
+        memset(dp, -1,sizeof(dp));
         int count = 0;
-        
-        // Check all possible substrings
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(palindromSubstr(i, j, s)){
-                    count++;
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                bool isPalindrome = rec(i, j, s);
+                if(isPalindrome){
+                    count+=1;
                 }
             }
         }
