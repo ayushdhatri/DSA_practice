d1eb9c17cc21ca02b9c01f42b3e7cbd4be77fa2b
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2026-09-10 16:59:57 +0530

    Time: 174 ms (21.4%), Space: 13.9 MB (38.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,69 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
-    struct manachar {
-        vector<int> p;
+int dp[1010][1010];
+bool rec(int startIndex, int endIndex, string &s){
+    // pruning
 
-        void runManachar(string t) {
-            int n = t.size();
-            p.assign(n, 1);
-            int l = 1, r = 1;
+    // base case
+    if(startIndex >= endIndex){
+        return true;
+    }
 
-            for (int i = 1; i < n; i++) {
-                if(i < r)
-                    p[i] = max(0, min(r - i, p[r - i + l]));
+    // cache
+    if(dp[startIndex][endIndex] != -1)return dp[startIndex][endIndex];
 
-                // BUG FIX 1: Use 'while' instead of 'if'
-                // BUG FIX 2: Correct lower bound check to (i - p[i] >= 0)
-                while (i - p[i] >= 0 && i + p[i] < n && t[i + p[i]] == t[i - p[i]]) {
-                    p[i]++;
-                }
+    // transition
+    bool isPalindrome = 0;
+    if(s[startIndex] == s[endIndex]){
+        isPalindrome = rec(startIndex+1, endIndex-1, s);
+    }
+   
+    // save and return
+    return dp[startIndex][endIndex] = isPalindrome;
 
-                if (i + p[i] > r) {
-                    l = i - p[i];
-                    r = i + p[i];
-                }
-            }
-        }
-
-        // BUG FIX 3: Change return type from void to vector<int>
-        vector<int> build(string s) {
-            // BUG FIX 4: Properly interleave characters with '#'
-            string t="";
-            for(auto x: s)
-            {
-                t+=(string("#") + x);
-            }
-            runManachar(t+"#");
-            return this->p;
-        }
-    };
-
+}
     string longestPalindrome(string s) {
-      
+        int n = s.size();
+        memset(dp, -1, sizeof(dp));
+        int startIndex = -1, endIndex = -1;
+        int maxLen = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                bool isPalindrome = rec(i, j, s);
+                int len = j-i+1;
+                if(isPalindrome && len > maxLen){
+                    maxLen = len;
+                    startIndex = i;endIndex = j;
+                }
 
-        manachar m1;
-        vector<int> manArr = m1.build(s);
-        cout<<manArr.size()<<endl;
-        int max_radius = 0;
-        int centerIndex = 0;
-
-        // Find center index with the maximum radius
-        for (int i = 0; i < manArr.size(); i++) {
-            if (manArr[i] > max_radius) {
-                max_radius = manArr[i];
-                centerIndex = i;
             }
         }
-
-        // Length of palindrome in original string
-        int max_len = max_radius - 1;
-
-        // Formula to convert index from transformed string 't' back to original string 's'
-        int startIndex = (centerIndex - max_len) / 2;
-        cout<<startIndex<<" "<<max_len;
-        return s.substr(startIndex, max_len);
-    
+        string ans = "";
+        for(int index = startIndex;index<=endIndex;index++){
+            ans+=s[index];
+        }
+        return ans;
     }
 };
